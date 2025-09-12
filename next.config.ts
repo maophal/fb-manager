851333286bb729fafcb4a846301dea75acfd023d
Tayes <tayes@Phals-Mac-mini.local>
2025-09-12 19:11:31 +0700

    fix error : build on production

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['platform-lookaside.fbsbx.com', 'scontent.xx.fbcdn.net', 'scontent.fpnh10-1.fna.fbcdn.net'],
   },
+  transpilePackages: ["geist"],
   /* config options here */
 };
 
