ed0aadba6601f4d9602e234b214f1c397c32958a
Tayes <tayes@Phals-Mac-mini.local>
2025-09-16 15:58:59 +0700

    update: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['platform-lookaside.fbsbx.com', 'scontent.xx.fbcdn.net', 'scontent.fpnh10-1.fna.fbcdn.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'platform-lookaside.fbsbx.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.fpnh10-1.fna.fbcdn.net',
+      },
+    ],
   },
   transpilePackages: ["geist"],
   /* config options here */
